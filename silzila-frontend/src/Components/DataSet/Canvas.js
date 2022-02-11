dd145cf2704bf9884ecbd48a67825b459f8f79b5
Saravanan <sarvan.rb@gmail.com>
2022-02-11 02:26:31 -0500

    Canvas & table skeleton done

--- silzila-frontend/src/Components/DataSet/Canvas.js
+++ silzila-frontend/src/Components/DataSet/Canvas.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { connect } from "react-redux";
+import BottomBar from "./BottomBar";
+import "./Dataset.css";
+import Tables from "./Tables";
 
-const Canvas = () => {
-    return <div className="canvas">Canvas</div>;
+const Canvas = ({
+    // state
+    tempTable,
+    arrows,
+    arrowType,
+    token,
+
+    // dispatch
+    addArrows,
+    clickOnArrow,
+    setArrowType,
+    setArrows,
+    resetArrows,
+    resetState,
+}) => {
+    return (
+        <div className="canvas">
+            <div className="canvasStyle" id="canvasTableArea">
+                {tempTable &&
+                    tempTable.map((table) => {
+                        return (
+                            <Tables
+                                tableData={table}
+
+                                // addArrow={addArrow}
+                            />
+                        );
+                    })}
+            </div>
+            <BottomBar />
+        </div>
+    );
 };
 
-export default Canvas;
+const mapStateToProps = (state) => {
+    return {
+        tempTable: state.dataSetState.tempTable,
+        arrows: state.dataSetState.arrows,
+        arrowType: state.dataSetState.arrowType,
+        token: state.isLogged.accessToken,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addArrows: (arrow) => dispatch({ type: "ADD_ARROWS", payload: arrow }),
+        clickOnArrow: (payload) => dispatch({ type: "CLICK_ON_ARROW", payload: payload }),
+        setArrowType: (payload) => dispatch({ type: "SET_ARROW_TYPE", payload: payload }),
+        setArrows: (pl) => dispatch({ type: "SET_ARROWS", payload: pl }),
+        resetArrows: () => dispatch({ type: "RESET_ARROWS_ARRAY" }),
+        resetState: () => dispatch({ type: "RESET_STATE" }),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Canvas);
---
+++ silzila-frontend/src/Components/DataSet/Tables.js
@@ -0,0 +1,121 @@
+import { Button, tableBodyClasses, TextField } from "@mui/material";
+import React, { useRef, useState } from "react";
+import Draggable from "react-draggable";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { connect } from "react-redux";
+
+const Tables = ({
+    // props
+    tableData,
+
+    // state
+    tableList,
+    tempTable,
+    arrows,
+    actions,
+
+    // dipatch
+}) => {
+    const dragRef = useRef();
+
+    let tableItems = tableData.columns;
+    let tableId = tableData.tableName;
+    let tableTitle = tableData.tableName;
+    let alias = tableData.alias;
+
+    const [inputField, setInputField] = useState(false);
+    const [newName, setNewName] = useState("");
+    const [selectedTable, setSelectedTable] = useState();
+    const [open, setOpen] = useState(false);
+
+    const changeTableName = (tableName) => {
+        const newTable = [...tempTable].map((tab) => {
+            if (tab.alias === tableName) {
+                tab.alias = newName;
+            }
+            return tab;
+        });
+        setNewName("");
+        setInputField(false);
+    };
+
+    return (
+        <Draggable ref={dragRef} handle={`#${tableId}`} bounds="#canvasTableArea">
+            <div className="draggableTable">
+                {inputField ? (
+                    <div>
+                        <TextField
+                            variant="standard"
+                            id="name"
+                            className="inputArea"
+                            value={newName}
+                            onChange={(e) => {
+                                e.preventDefault();
+                                setNewName(e.target.value);
+                            }}
+                        />
+                        <Button
+                            type="button"
+                            className="button_Ok"
+                            onClick={() => changeTableName(alias)}
+                        >
+                            Ok
+                        </Button>
+                    </div>
+                ) : (
+                    <div className="draggableBoxTitle" id={tableId}>
+                        <div
+                            onDoubleClick={() => {
+                                setInputField(true);
+                                setNewName(alias);
+                                setTimeout(() => {
+                                    // selectText();
+                                }, 10);
+                            }}
+                        >
+                            {alias}
+                        </div>
+                        <div>
+                            <MoreVertIcon
+                                style={{ float: "right" }}
+                                onClick={() => {
+                                    setSelectedTable(tableTitle);
+                                    setOpen(true);
+                                }}
+                            />
+                        </div>
+                    </div>
+                )}
+                {tableItems.map((item, index) => {
+                    return (
+                        <div>{item.column_name}</div>
+                        // <Columns
+                        // columnName={item.column_name}
+                        //     tableName={tableTitle}
+                        //     itemType={item.data_type}
+                        //     index={index}
+                        //     {...{
+                        //         addArrow,
+                        //         handler: "right",
+                        //         itemId: item.uid,
+                        //         tableId: tableId,
+                        //         dragRef,
+                        //     }}
+                        //     />
+                    );
+                })}
+            </div>
+        </Draggable>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        tableList: state.dataSetState.tables,
+        tempTable: state.dataSetState.tempTable,
+        arrows: state.dataSetState.arrows,
+        actions: state.dataSetState.actions,
+    };
+};
+
+export default connect(mapStateToProps, null)(Tables);
