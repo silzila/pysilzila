9aabb857c6046fc626c472146ff385010c400f5b
sarvanrb <sarvan.rb@gmail.com>
2022-04-14 01:05:26 -0400

    Gnkasthuri graphandchart controls filters (#30)
    * Arrow rendering while adding new table fixed.  New todos added
    
    * Minor fixes
    
    * charts added and right column menu changed
    
    * Chart Control area style altered, few bug fixes
    
    Co-authored-by: gnkasthuri <gkasthuricse16@gmail.com>

--- silzila-frontend/src/Components/Charts/PieChart.js
+++ silzila-frontend/src/Components/Charts/PieChart.js
@@ -1,4 +1,5 @@
 import ReactEcharts from "echarts-for-react";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 const PieChart = ({
@@ -8,17 +9,36 @@ const PieChart = ({
 
 	//state
 	chartProp,
+	chartControls,
 }) => {
-	var property = chartProp.properties[propKey];
-	console.log(property, "+++++ PROPERTY +++++");
+	// var property = chartProp.properties[propKey];
+	var property = chartControls.properties[propKey];
+
 	let chartData = property.chartData ? property.chartData.result : "";
 	console.log(chartData, "+++++ chartData +++++");
-	// const dimension = Object.keys(chartData[0])
 
-	return (
-		<>
-			{chartData ? (
+	var seriesObj = { type: "pie" };
+
+	const [seriesData, setSeriesData] = useState([]);
+
+	useEffect(() => {
+		var seriesDataTemp = [];
+		if (chartData) {
+			for (let i = 0; i < Object.keys(chartData[0]).length - 1; i++) {
+				seriesDataTemp.push(seriesObj);
+			}
+			setSeriesData(seriesDataTemp);
+		}
+	}, [chartData]);
+
+	console.log(seriesData);
+
+	// TODO: Priority 1 - Data not rendering properly. It shows dimension value instead of measure when dimension is Year
+	const RenderChart = () => {
+		return (
+			<>
 				<ReactEcharts
+					theme={property.colorScheme}
 					style={{
 						padding: "1rem",
 						width: graphDimension.width,
@@ -26,8 +46,24 @@ const PieChart = ({
 						overflow: "hidden",
 					}}
 					option={{
-						legend: {},
-						tooltip: {},
+						legend: {
+							type: "scroll",
+							show: property.legendOptions?.showLegend,
+							itemHeight: property.legendOptions?.symbolHeight,
+							itemWidth: property.legendOptions?.symbolWidth,
+							itemGap: property.legendOptions?.itemGap,
+
+							left: property.legendOptions?.position?.left,
+							top: property.legendOptions?.position?.top,
+							orient: property.legendOptions?.orientation,
+						},
+						grid: {
+							left: `${property.chartMargin.left}%`,
+							right: `${property.chartMargin.right}%`,
+							top: `${property.chartMargin.top}%`,
+							bottom: `${property.chartMargin.bottom}%`,
+						},
+						tooltip: { show: property.mouseOver.enable },
 						dataset: {
 							dimensions: Object.keys(chartData[0]),
 							source: chartData,
@@ -37,15 +73,16 @@ const PieChart = ({
 						series: [{ type: "pie" }],
 					}}
 				/>
-			) : (
-				""
-			)}
-		</>
-	);
+			</>
+		);
+	};
+	return <>{chartData ? <RenderChart /> : ""}</>;
 };
+
 const mapStateToProps = (state) => {
 	return {
 		chartProp: state.chartProperties,
+		chartControls: state.chartControls,
 	};
 };
 
--- silzila-frontend/src/Components/DataViewer/DataViewerMiddle.js
+++ silzila-frontend/src/Components/DataViewer/DataViewerMiddle.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import ChartAxes from "../ChartAxes/ChartAxes";
-import ChartControls from "../ChartOptions/ChartControls";
 import GraphArea from "../GraphArea/GraphArea";
 import "./dataViewerMiddle.css";
+import chartControlIcon from "../../assets/chart-control-icon.svg";
+import settingsIcon from "../../assets/charts_theme_settings_icon.svg";
+import FilterIcon from "../../assets/filter_icon.svg";
+import ChartControlObjects from "../ChartOptions/ChartControlObjects";
+import ControlDetail from "../ChartOptions/ControlDetail";
+import ChartTypes from "../ChartOptions/ChartTypes";
 
 const DataViewerMiddle = ({
 	// props
@@ -14,26 +19,62 @@ const DataViewerMiddle = ({
 	chartProp,
 }) => {
 	var propKey = `${tabId}.${tileId}`;
+	const rmenu = [
+		{ name: "Charts", icon: chartControlIcon },
+		{ name: "Chart controls", icon: settingsIcon },
+		{ name: "Filter", icon: FilterIcon },
+	];
 
-	const rightMenu = ["Chart Controls", "Filter"];
-	const [selectedMenu, setMenu] = useState("Chart Controls");
+	const [selectedMenu, setMenu] = useState("");
 
-	const renderMenu = () =>
-		rightMenu.map((option) => {
-			return (
-				<button
-					key={option}
-					className={
-						option === selectedMenu ? "menuRadioButtonSelected" : "menuRadioButton"
+	const renderMenu = rmenu.map((rm) => {
+		return (
+			<img
+				key={rm.name}
+				className={rm.name === selectedMenu ? "controlsIcon selectedIcon" : "controlsIcon"}
+				src={rm.icon}
+				alt={rm.name}
+				onClick={() => {
+					if (selectedMenu === rm.name) {
+						setMenu("");
+					} else {
+						setMenu(rm.name);
 					}
-					value={option}
-					onClick={(e) => setMenu(e.target.value)}
-					title={option}
-				>
-					{option}
-				</button>
-			);
-		});
+				}}
+				title={rm.name}
+			/>
+		);
+	});
+
+	const controlDisplayed = () => {
+		switch (selectedMenu) {
+			case "Charts":
+				return (
+					<div className="rightColumnControlsAndFilters">
+						<div className="axisTitle">Charts</div>
+						<ChartTypes propKey={propKey} />
+					</div>
+				);
+
+			case "Chart controls":
+				return (
+					<div className="rightColumnControlsAndFilters">
+						<div className="axisTitle">Charts Controls</div>
+						<ChartControlObjects />
+						<ControlDetail />
+					</div>
+				);
+
+			case "Filter":
+				return (
+					<div className="rightColumnControlsAndFilters">
+						<div className="axisTitle">Filters</div>
+					</div>
+				);
+			default:
+				return null;
+		}
+	};
 
 	return (
 		<div className="dataViewerMiddle">
@@ -41,12 +82,9 @@ const DataViewerMiddle = ({
 
 			<GraphArea />
 
-			<div className="rightColumn ">
-				<div className="radioButtonsUserMenu">{renderMenu()}</div>
-				{
-					selectedMenu === "Chart Controls" ? <ChartControls propKey={propKey} /> : null
-					// <ChartUserFilterContainer propKey={propKey} chartProp={chartProp} fileId={fileId} userFilterGroup={userFilterGroup} />
-				}
+			<div className="rightColumn">
+				{controlDisplayed()}
+				<div className="rightColumnMenu">{renderMenu}</div>
 			</div>
 		</div>
 	);
