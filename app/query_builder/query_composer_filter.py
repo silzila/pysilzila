db2a620a3395487bc97479abb756766acf25683b
silzila <mail@thebalu.com>
2022-04-17 15:55:43 +0530

    API - Filter Panel Dropped column - populate options V1

--- app/query_builder/query_composer_filter.py
+++ app/query_builder/query_composer_filter.py
@@ -1,4 +1,3 @@
-# from turtle import st
 from ..data_set import schema
 from ..data_connection import engine
 from .sql_dialect.filter_postgres import get_filter_values_pg
@@ -13,11 +12,21 @@ async def compose_query(req: schema.ColumnFilter, dc_uid: str, ds_uid: str, vend
     req = req.dict()
     # print("request ========", req)
     data_schema = await engine.get_data_schema(dc_uid, ds_uid)
-    table = list(filter(
-        lambda obj: obj["id"] == req['table_id'], data_schema["tables"]))[0]
-    FROM_TBL = f"{table['schema_name']}.{table['table_name']} AS {table['id']}"
-
     QUERY = ""
+    FROM_TBL = ""
+    if req['regular_filter'] is not None and req['regular_filter']['table_id'] and \
+            req['regular_filter']['field_name'] and req['regular_filter']['data_type']:
+        table = list(filter(
+            lambda obj: obj["id"] == req['regular_filter']['table_id'], data_schema["tables"]))[0]
+        FROM_TBL = f"{table['schema_name']}.{table['table_name']} AS {table['id']}"
+
+    elif req['relative_filter'] is not None and req['relative_filter']['table_id'] and \
+            req['relative_filter']['field_name'] and req['relative_filter']['data_type']:
+        table = list(filter(
+            lambda obj: obj["id"] == req['relative_filter']['table_id'], data_schema["tables"]))[0]
+        FROM_TBL = f"{table['schema_name']}.{table['table_name']} AS {table['id']}"
+    else:
+        FROM_TBL = ""
     ####################### Query changes as per dialect ############################
     if vendor_name == 'postgresql':
         QUERY = await get_filter_values_pg(req, FROM_TBL)
@@ -26,3 +35,10 @@ async def compose_query(req: schema.ColumnFilter, dc_uid: str, ds_uid: str, vend
     elif vendor_name == 'mssql':
         QUERY = await get_filter_values_mssql(req, FROM_TBL)
     return QUERY
+
+
+# for getting date of today, yesterday, tomorrow
+# used when dropping date, timestamp fields in filter and choose Relative span
+# and choose today, yesterday, tomorrow
+# if user chooses yesterday and today's date is 2022-12-30 then the API will give 2022-12-29
+# async def compose_query_today_date(day: str, dc_uid: str, ds_uid: str, vendor_name: str):
--- app/query_builder/sql_dialect/filter_postgres.py
+++ app/query_builder/sql_dialect/filter_postgres.py
@@ -1,95 +1,122 @@
 from fastapi import HTTPException
 
 
-'''
-This function is used to to show filter options when a user drags and drops a field into filter
-'''
-
+# This function is used to to show filter options when a user drags and drops a field into filter
+# req parameter is the request object - contains only one field
+# FROM_TBL parameter = schema_name.table_name.column_name as alias_name
 
 async def get_filter_values_pg(req, FROM_TBL):
+    ##############################################
     # get distinct values - text & number fields
-    if req['data_type'] in ('text', 'boolean') or (req['data_type']
-                                                   in ('integer', 'decimal') and req['filter_type'] == 'select members'):
-        _select = f"{req['table_id']}.{req['field_name']}"
-        QUERY = f"SELECT DISTINCT \n\t{_select}\nFROM\n\t{FROM_TBL}\n\nORDER BY\n\t1"
-    # range values - number fields
-    elif req['data_type'] in ('integer', 'decimal') and req['filter_type'] == 'select range':
-        _min = f"SELECT MIN({req['table_id']}.{req['field_name']}) AS colmn\nFROM\n\t{FROM_TBL}"
-        _max = f"SELECT MAX({req['table_id']}.{req['field_name']}) AS colmn\nFROM\n\t{FROM_TBL}"
-        QUERY = f"{_min}\nUNION ALL\n{_max}\nORDER BY\n\t1"
-    # get DATE dictinct values
-    elif req['data_type'] in ('date', 'timestamp'):
-        if req['filter_type'] == 'select members':
+    ##############################################
+    if req['regular_filter'] is not None:
+        filter = req['regular_filter']
+        if (filter['data_type'] in ('text', 'boolean') or (filter['data_type']
+                                                           in ('integer', 'decimal') and filter['filter_type'] == 'pick_from_list')):
+            _select = f"{filter['table_id']}.{filter['field_name']}"
+            QUERY = f"SELECT DISTINCT {_select} FROM {FROM_TBL} ORDER BY 1"
+        ##############################################
+        # range values - number fields
+        ##############################################
+        elif filter['data_type'] in ('integer', 'decimal') and filter['filter_type'] == 'search_condition':
+            _min = f"SELECT MIN({filter['table_id']}.{filter['field_name']}) AS colmn FROM {FROM_TBL}"
+            _max = f"SELECT MAX({filter['table_id']}.{filter['field_name']}) AS colmn FROM {FROM_TBL}"
+            QUERY = f"{_min} UNION ALL {_max} ORDER BY 1"
 
-            if req['aggr'] == 'year':
-                _select = f"EXTRACT(YEAR FROM {req['table_id']}.{req['field_name']})::INTEGER AS colmn"
-                QUERY = f"SELECT DISTINCT\n\t{_select}\nFROM\n\t{FROM_TBL}\n\nORDER BY\n\t1"
+        ##############################################
+        # get DATE dictinct values
+        ##############################################
+        elif filter['data_type'] in ('date', 'timestamp'):
+            ###########################
+            # dictinct values
+            ###########################
+            if filter['filter_type'] == 'pick_from_list':
+                if filter['time_grain'] == 'year':
+                    _select = f"EXTRACT(YEAR FROM {filter['table_id']}.{filter['field_name']})::INTEGER AS colmn"
+                    QUERY = f"SELECT DISTINCT {_select} FROM {FROM_TBL} ORDER BY 1"
 
-            elif req['aggr'] == 'month':
-                field1 = f"EXTRACT(MONTH FROM {req['table_id']}.{req['field_name']})::INTEGER AS index"
-                field2 = f"TRIM(TO_CHAR({req['table_id']}.{req['field_name']}, 'Month')) AS name"
-                QUERY = f"SELECT DISTINCT\n\t{field1}, \n\t{field2}\nFROM\n\t{FROM_TBL}\n\nORDER BY\n\t1"
+                elif filter['time_grain'] == 'quarter':
+                    field = f"CONCAT('Q', EXTRACT(QUARTER FROM {filter['table_id']}.{filter['field_name']})::INTEGER) AS colmn"
+                    QUERY = f"SELECT DISTINCT {field} FROM {FROM_TBL} ORDER BY 1"
 
-            elif req['aggr'] == 'quarter':
-                field1 = f"EXTRACT(QUARTER FROM {req['table_id']}.{req['field_name']})::INTEGER AS index"
-                field2 = f"CONCAT('Q', EXTRACT(QUARTER FROM {req['table_id']}.{req['field_name']})::INTEGER) AS colmn"
-                QUERY = f"SELECT DISTINCT\n\t{field1}, \n\t{field2}\nFROM\n\t{FROM_TBL}\n\nORDER BY\n\t1"
+                elif filter['time_grain'] == 'month':
+                    field1 = f"EXTRACT(MONTH FROM {filter['table_id']}.{filter['field_name']})::INTEGER"
+                    field2 = f"TRIM(TO_CHAR({filter['table_id']}.{filter['field_name']}, 'Month'))"
+                    QUERY = f"SELECT {field2} AS mnth FROM {FROM_TBL} GROUP BY {field1},{field2} ORDER BY {field1}"
 
-            elif req['aggr'] == 'dayofweek':
-                field1 = f"EXTRACT(DOW FROM {req['table_id']}.{req['field_name']})::INTEGER +1 AS dayofweek_index"
-                field2 = f"TRIM(TO_CHAR({req['table_id']}.{req['field_name']}, 'Day')) AS dayofweek_name"
-                QUERY = f"SELECT DISTINCT\n\t{field1}, \n\t{field2}\nFROM\n\t{FROM_TBL}\n\nORDER BY\n\t1"
+                elif filter['time_grain'] == 'yearquarter':
+                    field = f"CONCAT(TO_CHAR({filter['table_id']}.{filter['field_name']}, 'YYYY'), '-Q', TO_CHAR({filter['table_id']}.{filter['field_name']}, 'Q'))"
+                    QUERY = f"SELECT DISTINCT {field} FROM {FROM_TBL} ORDER BY 1"
 
-            elif req['aggr'] == 'day':
-                _select = f"EXTRACT(DAY FROM {req['table_id']}.{req['field_name']})::INTEGER AS colmn"
-                QUERY = f"SELECT DISTINCT \n\t{_select}\nFROM\n\t{FROM_TBL}\n\nORDER BY\n\t1"
+                elif filter['time_grain'] == 'yearmonth':
+                    field = f"TO_CHAR({filter['table_id']}.{filter['field_name']}, 'YYYY-MM')"
+                    QUERY = f"SELECT DISTINCT {field} FROM {FROM_TBL} ORDER BY 1"
 
-            else:
-                raise HTTPException(
-                    status_code=400, detail="Aggregation is wrong")
+                elif filter['time_grain'] == 'date':
+                    field = f"DATE({filter['table_id']}.{filter['field_name']})"
+                    QUERY = f"SELECT DISTINCT {field} FROM {FROM_TBL} ORDER BY 1"
+                # in postgres, dayofweek starts from 0. So we add +1 to be consistent across DB
+                elif filter['time_grain'] == 'dayofweek':
+                    field1 = f"EXTRACT(DOW FROM {filter['table_id']}.{filter['field_name']})::INTEGER +1"
+                    field2 = f"TRIM(TO_CHAR({filter['table_id']}.{filter['field_name']}, 'Day'))"
+                    QUERY = f"SELECT {field2} AS dayofweek FROM {FROM_TBL} GROUP BY {field1},{field2} ORDER BY {field1}"
 
-        # get DATE range values
-        elif req['filter_type'] == 'select range':
-            if req['aggr'] == 'year':
-                _col = f"EXTRACT(YEAR FROM {req['table_id']}.{req['field_name']})::INTEGER"
-                _min = f"SELECT MIN({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                _max = f"SELECT MAX({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                QUERY = f"{_min}\nUNION ALL\n{_max}\nORDER BY\n\t1"
+                elif filter['time_grain'] == 'dayofmonth':
+                    _select = f"EXTRACT(DAY FROM {filter['table_id']}.{filter['field_name']})::INTEGER AS colmn"
+                    QUERY = f"SELECT DISTINCT {_select} FROM {FROM_TBL} ORDER BY 1"
 
-            elif req['aggr'] == 'month':
-                _col = f"EXTRACT(MONTH FROM {req['table_id']}.{req['field_name']})::INTEGER"
-                _min = f"SELECT MIN({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                _max = f"SELECT MAX({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                QUERY = f"{_min}\nUNION ALL\n{_max}\nORDER BY\n\t1"
+                else:
+                    raise HTTPException(
+                        status_code=400, detail="Aggregation is wrong in Date pick from list")
 
-            elif req['aggr'] == 'quarter':
-                _col = f"EXTRACT(QUARTER FROM {req['table_id']}.{req['field_name']})::INTEGER"
-                _min = f"SELECT MIN({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                _max = f"SELECT MAX({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                QUERY = f"{_min}\nUNION ALL\n{_max}\nORDER BY\n\t1"
+            ###########################
+            # range values
+            ###########################
+            elif filter['filter_type'] == 'search_condition':
+                if filter['time_grain'] == 'year':
+                    _col = f"EXTRACT(YEAR FROM {filter['table_id']}.{filter['field_name']})::INTEGER"
+                    _min = f"SELECT MIN({_col}) AS colmn FROM {FROM_TBL}"
+                    _max = f"SELECT MAX({_col}) AS colmn FROM {FROM_TBL}"
+                    QUERY = f"{_min} UNION ALL {_max} ORDER BY 1"
 
-            elif req['aggr'] == 'dayofweek':
-                _col = f"EXTRACT(DOW FROM {req['table_id']}.{req['field_name']})::INTEGER + 1"
-                _min = f"SELECT MIN({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                _max = f"SELECT MAX({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                QUERY = f"{_min}\nUNION ALL\n{_max}\nORDER BY\n\t1"
+                elif filter['time_grain'] == 'month':
+                    _col = f"EXTRACT(MONTH FROM {filter['table_id']}.{filter['field_name']})::INTEGER"
+                    _min = f"SELECT MIN({_col}) AS colmn FROM {FROM_TBL}"
+                    _max = f"SELECT MAX({_col}) AS colmn FROM {FROM_TBL}"
+                    QUERY = f"{_min} UNION ALL {_max} ORDER BY 1"
 
-            elif req['aggr'] == 'day':
-                _col = f"EXTRACT(DAY FROM {req['table_id']}.{req['field_name']})::INTEGER"
-                _min = f"SELECT MIN({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                _max = f"SELECT MAX({_col}) AS colmn\nFROM\n\t{FROM_TBL}"
-                QUERY = f"{_min}\nUNION ALL\n{_max}\nORDER BY\n\t1"
-            else:
-                raise HTTPException(
-                    status_code=400, detail="Aggregation is wrong")
-        else:
-            raise HTTPException(
-                status_code=400, detail="Filter Type is wrong")
+                elif filter['time_grain'] == 'quarter':
+                    _col = f"EXTRACT(QUARTER FROM {filter['table_id']}.{filter['field_name']})::INTEGER"
+                    _min = f"SELECT MIN({_col}) AS colmn FROM {FROM_TBL}"
+                    _max = f"SELECT MAX({_col}) AS colmn FROM {FROM_TBL}"
+                    QUERY = f"{_min} UNION ALL {_max} ORDER BY 1"
+
+                elif filter['time_grain'] == 'date':
+                    _col = f"DATE({filter['table_id']}.{filter['field_name']})"
+                    _min = f"SELECT MIN({_col}) AS colmn FROM {FROM_TBL}"
+                    _max = f"SELECT MAX({_col}) AS colmn FROM {FROM_TBL}"
+                    QUERY = f"{_min} UNION ALL {_max} ORDER BY 1"
+
+                elif filter['time_grain'] == 'dayofweek':
+                    _col = f"EXTRACT(DOW FROM {filter['table_id']}.{filter['field_name']})::INTEGER + 1"
+                    _min = f"SELECT MIN({_col}) AS colmn FROM {FROM_TBL}"
+                    _max = f"SELECT MAX({_col}) AS colmn FROM {FROM_TBL}"
+                    QUERY = f"{_min} UNION ALL {_max} ORDER BY 1"
+
+                elif filter['time_grain'] == 'dayofmonth':
+                    _col = f"EXTRACT(DAY FROM {filter['table_id']}.{filter['field_name']})::INTEGER"
+                    _min = f"SELECT MIN({_col}) AS colmn FROM {FROM_TBL}"
+                    _max = f"SELECT MAX({_col}) AS colmn FROM {FROM_TBL}"
+                    QUERY = f"{_min} UNION ALL {_max} ORDER BY 1"
+                else:
+                    raise HTTPException(
+                        status_code=400, detail="Aggregation is wrong")
+
     else:
         raise HTTPException(
             status_code=400, detail="Data Type or Filter Type is wrong")
 
-    print("\n===============QUERY=========================================")
+    print(" ===============QUERY=========================================")
     print(QUERY)
 
     return QUERY
