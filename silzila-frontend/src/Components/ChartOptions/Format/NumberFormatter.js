d7dd7b08f28ed0e788323e3e5e5a7e03524d60af
Saravanan <sarvan.rb@gmail.com>
2022-06-01 11:42:54 -0400

    Added comments to Number formatter

--- silzila-frontend/src/Components/ChartOptions/Format/NumberFormatter.js
+++ silzila-frontend/src/Components/ChartOptions/Format/NumberFormatter.js
@@ -1,3 +1,7 @@
+// Helper function to format number
+
+// Given a number this function returns an abbreviated number
+// Eg 1000000 will be returned as 1M
 export const formatNumberWithAbbrev = (value, digits) => {
 	return Math.abs(Number(value)) >= 1.0e9
 		? (Math.abs(Number(value)) / 1.0e9).toFixed(digits) + "B"
@@ -10,16 +14,22 @@ export const formatNumberWithAbbrev = (value, digits) => {
 		: Math.abs(Number(value));
 };
 
+// Given a number this function returns comma separated number
+// Eg 1000000 will be returned as 1,000,000
 export const formatNumberWithComma = (value) => {
 	var commas = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	return commas;
 };
 
+// Function that formats numbers in labels for a chart
+// Rounding off digits, adding commas or abbreviations, add a currency Symbol, etc
 export const formatChartLabelValue = (chartControl, value) => {
+	// If Rounding enabled, returns a rounded value
 	if (chartControl.formatOptions.labelFormats.enableRounding) {
 		value = Number(value).toFixed(chartControl.formatOptions.labelFormats.roundingDigits);
 	}
 
+	// Returns an appreviated value along with required rounding off
 	if (chartControl.formatOptions.labelFormats.numberSeparator === "Abbrev") {
 		var text = value.toString();
 		var index = text.indexOf(".");
@@ -35,27 +45,29 @@ export const formatChartLabelValue = (chartControl, value) => {
 		);
 	}
 
+	// Returns a comma separated value of number
 	if (chartControl.formatOptions.labelFormats.numberSeparator === "Comma") {
 		value = formatNumberWithComma(value);
 	}
 
+	// Returns value with currency symbol of user's choice
 	if (chartControl.formatOptions.labelFormats.formatValue === "Currency")
 		value = `${chartControl.formatOptions.labelFormats.currencySymbol} ${value}`;
 
+	// Retuns value with a % suffix
 	if (chartControl.formatOptions.labelFormats.formatValue === "Percent") value = `${value} %`;
 
 	return value;
 };
 
+// Similar to above function. But formatting done for Y axis values in a chart
 export const formatChartYAxisValue = (chartControl, value) => {
-	// =========================================
-	// Format RoundOff
+	// If Rounding enabled, returns a rounded value
 	if (chartControl.formatOptions.yAxisFormats.enableRounding) {
 		value = Number(value).toFixed(chartControl.formatOptions.yAxisFormats.roundingDigits);
 	}
 
-	// =========================================
-	// Format number separator
+	// Returns an appreviated value along with required rounding off
 	if (chartControl.formatOptions.yAxisFormats.numberSeparator === "Abbrev") {
 		var text = value.toString();
 		var index = text.indexOf(".");
@@ -71,18 +83,17 @@ export const formatChartYAxisValue = (chartControl, value) => {
 		);
 	}
 
-	// =========================================
-	// Format Comma
+	// Returns a comma separated value of number
 	if (chartControl.formatOptions.yAxisFormats.numberSeparator === "Comma") {
 		value = formatNumberWithComma(value);
 	}
 
-	// =========================================
-	// Format Currency / Percentage
+	// Returns value with currency symbol of user's choice
 	if (chartControl.formatOptions.labelFormats.formatValue === "Currency") {
 		value = `${chartControl.formatOptions.labelFormats.currencySymbol} ${value}`;
 	}
 
+	// Retuns value with a % suffix
 	if (chartControl.formatOptions.labelFormats.formatValue === "Percent") {
 		value = `${value} %`;
 	}
@@ -90,15 +101,14 @@ export const formatChartYAxisValue = (chartControl, value) => {
 	return value;
 };
 
+// Similar to above function. But formatting done for X axis values in a chart
 export const formatChartXAxisValue = (chartControl, value) => {
-	// =========================================
-	// Format RoundOff
+	// If Rounding enabled, returns a rounded value
 	if (chartControl.formatOptions.xAxisFormats.enableRounding) {
 		value = Number(value).toFixed(chartControl.formatOptions.xAxisFormats.roundingDigits);
 	}
 
-	// =========================================
-	// Format number separator
+	// Returns an appreviated value along with required rounding off
 	if (chartControl.formatOptions.xAxisFormats.numberSeparator === "Abbrev") {
 		var text = value.toString();
 		var index = text.indexOf(".");
@@ -114,18 +124,17 @@ export const formatChartXAxisValue = (chartControl, value) => {
 		);
 	}
 
-	// =========================================
-	// Format Comma
+	// Returns a comma separated value of number
 	if (chartControl.formatOptions.xAxisFormats.numberSeparator === "Comma") {
 		value = formatNumberWithComma(value);
 	}
 
-	// =========================================
-	// Format Currency / Percentage
+	// Returns value with currency symbol of user's choice
 	if (chartControl.formatOptions.labelFormats.formatValue === "Currency") {
 		value = `${chartControl.formatOptions.labelFormats.currencySymbol} ${value}`;
 	}
 
+	// Retuns value with a % suffix
 	if (chartControl.formatOptions.labelFormats.formatValue === "Percent") {
 		value = `${value} %`;
 	}
